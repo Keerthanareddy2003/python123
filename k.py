41c1f2a2b67d35e47a6b2bd21afe7247162524a0
Keerthanareddy2003 <128774075+Keerthanareddy2003@users.noreply.github.com>
2024-10-28 20:44:58 +0530

    Add files via upload

---
+++ k.py
@@ -0,0 +1,8 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+   x=int(input("Enter the value of x="))
+   y=int(input("Enter the value of y="))
+   print("The sum of x+y = ", x+y)
+   print("The sub of x-y = ", x-y)
+   print("The mul of x*y = ", x*y)
+ 
